b428b584f41d848e89ae49f2b180d449ab651383
Simon Kellen <simonkellen@voo.be>
2019-10-21 14:44:17 +0200

    plagiat

--- src/Headers/read_write_loop.h
+++ src/Headers/read_write_loop.h
@@ -6,6 +6,9 @@
  * @sfd: The socket file descriptor. It is both bound and connected.
  * @return: as soon as stdin signals EOF
  */
+
+//Ce code a été repris du code du projet de l'année précédente. Les membres du groupe à l'époque étaient Matteo Snellings et moi-même, Simon Kellen.
+
 int read_write_loop(const int sfd, char *filename);
 
 #endif
